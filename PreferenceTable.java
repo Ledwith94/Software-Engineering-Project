2c028000aad4b6cdb4970a23061de62f57a7c4bc
Conor <conspain@hotmail.com>
2016-02-05 15:59:34 +0000

    Update PreferenceTable.java
    Now we have person objects and the token arguments going into them
    I thought this would be an easier way to do this rather than making massive vectors with all of the information.
    The vector stuff is still in there for the moment just in case, its just not being printed out

--- PreferenceTable.java
+++ PreferenceTable.java
@@ -7,12 +7,15 @@ import java.util.Vector;
 
 public class PreferenceTable 
 {
+	
+	static Person[] group = new Person[52];										// for 51 students and the top line 
 
 	private static Vector<Vector<String>> loadContentFromFile(String filePath) throws IOException
 	{
 		Vector<Vector<String>> vector = new Vector<Vector<String>>();			// create vector of vectors containing strings
 		Vector<String> vector2 = new Vector<String>();							// create vector of strings
 		File file = new File(filePath);											// set file path from inputted String
+		int g = 0;
 			
 		try (BufferedReader br = new BufferedReader(new FileReader(file))) 		// BufferedReader to read each line by line
 		{
@@ -25,12 +28,22 @@ public class PreferenceTable
 		    	
 		    	int l = tokenTest(tokens);										// count how many tokens there are
 		    	
+		    	Person add = new Person();
+		    	add.setName(tokens[0]);
+		    	add.setPreArranged(tokens[1]);
+		    	
 		    	for(i = 0; i < l; i++)
 		    	{
 		    		vector2.add(tokens[i]);										// add tokens to vector
+		    		if(i > 1)
+		    		{
+		    			add.addPref(tokens[i], i-2);
+		    		}
 		    	}
 		    	
 		    	vector2.add("\n");
+		    	group[g] = add;
+		    	g++;
 		    	
 		    	vector.add(vector2);											// add vector to vector of vectors
 		    }
@@ -49,7 +62,11 @@ public class PreferenceTable
 	public static void main(String[] args) throws IOException 
 	{
 		Vector<Vector<String>> preferences = loadContentFromFile("src/data.tsv");
-		System.out.println(preferences);
+		
+		for(int i = 1; i < group.length; i++)
+		{
+			System.out.println(group[i]);
+		}
 	}
 
 }
