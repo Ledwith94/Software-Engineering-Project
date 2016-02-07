a919685421fe66239ff820875a5aa8172cd91d6b
Conor <conspain@hotmail.com>
2016-02-07 00:35:27 +0000

    Removed everything vector related, didn't need them

--- PreferenceTable.java
+++ PreferenceTable.java
@@ -2,18 +2,16 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Vector;
-
+import java.util.ArrayList;
 
 public class PreferenceTable 
 {
-	
+	public static ArrayList<String> choices = new ArrayList<String>();
+		
 	static Person[] group = new Person[52];										// for 51 students and the top line 
 
-	private static Vector<Vector<String>> loadContentFromFile(String filePath) throws IOException
+	private static void loadContentFromFile(String filePath) throws IOException
 	{
-		Vector<Vector<String>> vector = new Vector<Vector<String>>();			// create vector of vectors containing strings
-		Vector<String> vector2 = new Vector<String>();							// create vector of strings
 		File file = new File(filePath);											// set file path from inputted String
 		int g = 0;
 			
@@ -21,35 +19,40 @@ public class PreferenceTable
 		{
 		    String line;
 		    int i = 0;
+		    int a = 0;
 		    while ((line = br.readLine()) != null) 								// until EOF
 			{
-		    	String[] tokens = new String[12];								// Set up string array
-		    	tokens = line.split("\\t");										// split line into tokens for string array
+		    	if (a == 0)
+    			{
+		    		// do nothing
+    			}
 		    	
-		    	int l = tokenTest(tokens);										// count how many tokens there are
-		    	
-		    	Person add = new Person();
-		    	add.setName(tokens[0]);
-		    	add.setPreArranged(tokens[1]);
-		    	
-		    	for(i = 0; i < l; i++)
+		    	else
 		    	{
-		    		vector2.add(tokens[i]);										// add tokens to vector
-		    		if(i > 1)
-		    		{
-		    			add.addPref(tokens[i], i-2);
-		    		}
+			    	String[] tokens = new String[12];								// Set up string array
+			    	tokens = line.split("\\t");										// split line into tokens for string array
+			    	
+			    	int l = tokenTest(tokens);										// count how many tokens there are
+			    	
+			    	Person add = new Person();										// create person object
+			    	add.setName(tokens[0]);											// add name to person
+			    	add.setPreArranged(tokens[1]);									// add pre-arranged value to person
+			    	
+			    	for(i = 0; i < l; i++)											// for all of the tokens 
+			    	{
+			    		if(i > 1)													// add preferences to preference vector, when i is not 0 or 1
+			    		{
+			    			add.addPref(tokens[i]);
+			    			addToChoiceArray(tokens[i]);
+			    		}
+			    	}
+			    	
+			    	group[g] = add;
+			    	g++;
 		    	}
-		    	
-		    	vector2.add("\n");
-		    	group[g] = add;
-		    	g++;
-		    	
-		    	vector.add(vector2);											// add vector to vector of vectors
+		    	a = 1;
 		    }
 		}
-		
-		return vector;															// return vector
 	}
 		
 	public static int tokenTest(String[] tokens)
@@ -58,15 +61,35 @@ public class PreferenceTable
 		return amount;															// return amount of tokens
 	}	
 	
+	public static void addToChoiceArray(String choice)
+	{
+		
+		if(choices.contains(choice))
+		{
+			// do nothing
+		}
+		
+		else
+		{
+			choices.add(choice);
+		}
+	}
 	
 	public static void main(String[] args) throws IOException 
 	{
-		Vector<Vector<String>> preferences = loadContentFromFile("src/data.tsv");
+		loadContentFromFile("src/data.tsv");
 		
-		for(int i = 1; i < group.length; i++)
+		for(int i = 0; i < group.length; i++)
 		{
 			System.out.println(group[i]);
 		}
+				
+		java.util.Collections.sort(choices);			// Alphabetize choices 
+		
+//		for(int i = 0; i < choices.size(); i++)
+//		{
+//			System.out.println(choices.get(i));
+//		}
 	}
 
 }
