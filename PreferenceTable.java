664b092d7a9d99ff0ab8802a462b669e6a4069a6
Conor <conspain@hotmail.com>
2016-02-17 14:20:34 +0000

    Update since assignment 2

--- PreferenceTable.java
+++ PreferenceTable.java
@@ -3,12 +3,15 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Hashtable;
 
-public class PreferenceTable 
+public class PreferenceTable
 {
 	public static ArrayList<String> choices = new ArrayList<String>();
 		
-	static Person[] group = new Person[52];										// for 51 students and the top line 
+	static StudentEntry[] group = new StudentEntry[52];										// for 51 students and the top line 
+	static Hashtable<String, StudentEntry> table = new Hashtable<String, StudentEntry>();	// hashtable for names and student entries
+	
 
 	private static void loadContentFromFile(String filePath) throws IOException
 	{
@@ -20,11 +23,11 @@ public class PreferenceTable
 		    String line;
 		    int i = 0;
 		    int a = 0;
-		    while ((line = br.readLine()) != null) 								// until EOF
+		    while ((line = br.readLine()) != null) 									// until EOF
 			{
 		    	if (a == 0)
     			{
-		    		// do nothing
+		    																		// do nothing for the top line 
     			}
 		    	
 		    	else
@@ -34,9 +37,9 @@ public class PreferenceTable
 			    	
 			    	int l = tokenTest(tokens);										// count how many tokens there are
 			    	
-			    	Person add = new Person();										// create person object
-			    	add.setName(tokens[0]);											// add name to person
-			    	add.setPreArranged(tokens[1]);									// add pre-arranged value to person
+			    	StudentEntry add = new StudentEntry();							// create StudentEntry object
+			    	add.setName(tokens[0]);											// add name to StudentEntry
+			    	add.setPreArranged(tokens[1]);									// add pre-arranged value to StudentEntry
 			    	
 			    	for(i = 0; i < l; i++)											// for all of the tokens 
 			    	{
@@ -47,7 +50,10 @@ public class PreferenceTable
 			    		}
 			    	}
 			    	
-			    	group[g] = add;
+			    	add.setStatedPrefs();											// store stated preferences
+			    	
+			    	group[g] = add;													// add student to group 
+			    	table.put(add.getName(), add);									// add student to hashtable
 			    	g++;
 		    	}
 		    	a = 1;
@@ -58,13 +64,13 @@ public class PreferenceTable
 	public static int tokenTest(String[] tokens)
 	{
 		int amount = tokens.length;
-		return amount;															// return amount of tokens
+		return amount;																// return amount of tokens
 	}	
 	
 	public static void addToChoiceArray(String choice)
 	{
 		
-		if(choices.contains(choice))
+		if(choices.contains(choice))												// if choice is already there, don't add
 		{
 			// do nothing
 		}
@@ -75,21 +81,63 @@ public class PreferenceTable
 		}
 	}
 	
+	public static ArrayList<StudentEntry> getAllStudentEntries()					// create list of all student entries
+	{
+		ArrayList<StudentEntry> list = new ArrayList<StudentEntry>();					
+		
+		for(int i = 0; i != group.length - 1; i++)
+		{
+			list.add(group[i]);														// add to list 
+		}
+		
+		return list;
+	}
+	
+	public static StudentEntry getEntryFor(String name)								// lookup hashtable and return student
+	{
+		StudentEntry student = new StudentEntry();
+		
+		if(table.containsKey(name))													// if name in hashtable
+		{
+			student = table.get(name);												// get student 
+		}
+		else
+		{
+			student = null;
+		}
+		
+		return student;
+	}
+	
 	public static void main(String[] args) throws IOException 
 	{
 		loadContentFromFile("src/data.tsv");
 		
-		for(int i = 0; i < group.length; i++)
-		{
-			System.out.println(group[i]);
-		}
-				
-		java.util.Collections.sort(choices);			// Alphabetize choices 
+		StudentEntry test = group[0];
 		
-//		for(int i = 0; i < choices.size(); i++)
-//		{
-//			System.out.println(choices.get(i));
-//		}
+		System.out.println("Name: " + test.getName());
+		System.out.println("Pre-arranged: " + test.getpreArranged());
+		System.out.println("Preferences: \n" + test.getPrefs());
+		System.out.println("Stated Preferences: " + test.getStatedPrefs()+"\n");
+		
+		ArrayList<StudentEntry> list = getAllStudentEntries();
+		System.out.println(list.toString());
+		
+		StudentEntry student = getEntryFor("Richard B. Riddick");					// getStudentEntry
+		
+		if(student == null)
+		{
+			System.out.println("Error, student entry null");
+		}
+		else
+		{
+			System.out.println("\ngetEntryFor success for "+student);
+		}
+		
+		//System.out.println("\n\n Hashtable: "+table.toString());
+				
+		java.util.Collections.sort(choices);										// Alphabetize list of choices 
+		
 	}
 
 }
