6941c84ab38895047f3707f8fc531daa5e653f55
Conor <conspain@hotmail.com>
2016-04-14 14:40:09 +0100

    Changed Energy Calculating Function
    Also added a copy function for use in Simulated Annealing

--- CandidateSolution.java
+++ CandidateSolution.java
@@ -1,47 +1,84 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map.Entry;
 import java.util.Random;
 
 public class CandidateSolution 
 {
-	@SuppressWarnings("unused")
 	private PreferenceTable prefTable;
 	private static Random RND = new Random();
 	private int energy;
 	private int projAllocationEnergy;
 
-	HashMap<String, CandidateAssignment> map = new HashMap<String, CandidateAssignment>();
-	HashMap<String, Integer> projects = new HashMap<String, Integer>();
+	private HashMap<String, CandidateAssignment> map = new HashMap<String, CandidateAssignment>();
+	
+	public HashMap<String, CandidateAssignment> getMap()
+		{return this.map;}
+	
+	public void updateMap(HashMap<String, CandidateAssignment> newMap)
+		{this.map = newMap;}
+	
+	public PreferenceTable getTable()
+		{return this.prefTable;}
+	
+	public void updateTable(PreferenceTable p)
+		{this.prefTable = p;}
+	
+	public CandidateSolution copySol()
+		{CandidateSolution copyTo = new CandidateSolution(null);
+		 copyTo.updateMap(this.getMap());
+		 copyTo.updateTable(this.prefTable);
+		 copyTo.calcTotalEnergy();
+		 return copyTo;}
 
-	public void initialize(PreferenceTable table)									// initialize method
-		{this.prefTable = table;}
+	public CandidateSolution(PreferenceTable table)									// initialize method
+		{this.prefTable = table;
+		 if(!(table == null))
+		 	{for(int i = 0; i < table.getStudents().size() - 1; i++)
+		 		{CandidateAssignment cand = new CandidateAssignment(table.getStudents().get(i));
+		 		 this.addAssignment(cand);}
+		 	}
+		}
 	
 	public void addAssignment(CandidateAssignment cand)
-	 {this.map.put(cand.getStudentEntry().getName(), cand);							// add cand assignment
-	  if(projects.containsKey(cand.getAssignedProject()))							// if project has been used before
-	  	{int a = projects.get(cand.getAssignedProject());							// get the amount of times
-	  	 projects.put(cand.getAssignedProject(), (a+1));}							// add 1
-	  else																			// if project not used yet
-	  	{projects.put(cand.getAssignedProject(), 1);}								// add project with int 1
-	  this.addEnergy(cand.getEnergy());
-	 }
+		{this.map.put(cand.getStudentEntry().getName(), cand);}						// add cand assignment
 	
 	public CandidateAssignment getSolutionFor(String name)							// get candidate assignment 
-		{if(map.containsKey(name)){return map.get(name);}							// return cand assignment if found
+		{if(this.map.containsKey(name)){return this.map.get(name);}							// return cand assignment if found
 		 return null;}																// else return null 
 	
 	public CandidateAssignment getRandomAssignment()
-		{List<String> keys = new ArrayList<String>(map.keySet());					// store string keys in a list
+		{List<String> keys = new ArrayList<String>(this.map.keySet());					// store string keys in a list
 		 return map.get(keys.get(RND.nextInt(keys.size())));}						// choose a key at random
 	
-	public void addEnergy(int e)
-		{this.energy += e;}
-	
-	public void addProjAllocationEnergy()
-		{for(int value : projects.values()) 
-			{if(value > 1){this.projAllocationEnergy += ((value-1)*100);}}			// if a project appears more than once, energy += 100*amount of times used more than once
-		 this.addEnergy(this.getAllocEnergy());}
+	public int calcTotalEnergy()
+	{
+		HashMap<String, Integer> projects = new HashMap<String, Integer>();
+		int energy = 0;
+		int projEnergy = 0;
+
+		for (Entry<String, CandidateAssignment> entry : this.map.entrySet())
+		{
+			CandidateAssignment cand = entry.getValue();
+		
+			if(projects.containsKey(cand.getAssignedProject()))								// if project has been used before
+		  		{int a = projects.get(cand.getAssignedProject());							// get the amount of times
+		  		 projects.put(cand.getAssignedProject(), (a+1));}							// add 1
+			else																			// if project not used yet
+		  		{projects.put(cand.getAssignedProject(), 1);}								// add project with int 1
+			
+			energy += cand.getEnergy();
+		}
+		
+		for(int value : projects.values()) 
+			{if(value > 1){projEnergy += ((value-1)*100);}}			// if a project appears more than once, energy += 100*amount of times used more than once
+		
+		this.projAllocationEnergy = projEnergy;
+		this.energy = energy + projEnergy;
+		
+		return this.energy;
+	}
 		
 	public int getEnergy()
 		{return this.energy;}
@@ -51,4 +88,5 @@ public class CandidateSolution
 	
 	public int getFitness()
 		{return this.getEnergy() * -1;}
+	
 }
